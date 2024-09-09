7ba747d3f06c2484a670db33f8adf140e9d72ca9
seanyoo333 <shineyou82@gmail.com>
2024-09-10 07:20:13 +0900

    first commit!

--- 6-1 card game.py
+++ 6-1 card game.py
@@ -1,9 +1,17 @@
 NUM_CARDS = 52
 
 def no_high(lst):
-    """Check if the list lst contains any high cards."""
-    high_cards = {'jack', 'queen', 'king', 'ace'}
-    return not any(card in high_cards for card in lst)
+    """lst는 카드를 나타내는 문자열들의 리스트
+    lst에 높은 카드가 없으면 True, 그렇지 않으면 False"""
+    if 'jack' in lst:
+        return False
+    if 'queen' in lst:
+        return False
+    if 'king' in lst:
+        return False
+    if 'ace' in lst:
+        return False
+    return True
 
 deck = []
 
@@ -18,9 +26,8 @@ for i in range(NUM_CARDS):
     card = deck[i]
     points = 0
     remaining = NUM_CARDS - i - 1
-
     if card == 'jack' and remaining >= 1 and no_high(deck[i+1:i+2]):
-        points  = 1
+        points = 1
     elif card == 'queen' and remaining >= 2 and no_high(deck[i+1:i+3]):
         points = 2
     elif card == 'king' and remaining >= 3 and no_high(deck[i+1:i+4]):
@@ -28,14 +35,15 @@ for i in range(NUM_CARDS):
     elif card == 'ace' and remaining >= 4 and no_high(deck[i+1:i+5]):
         points = 4
 
+
     if points > 0:
         print(f'Player {player} scores {points} point(s).')
 
     if player == 'A':
-        score_a += points
+        score_a = score_a + points
         player = 'B'
     else:
-        score_b += points
+        score_b = score_b + points
         player = 'A'
 
 print(f'Player A: {score_a} point(s).')
--- 6-2 action figures.py
+++ 6-2 action figures.py
@@ -1,24 +1,27 @@
 def read_boxes(n):
-    """n은 읽어야 하는 상자수, 각 상자는 액션 피규어의 높이를 담은 리스트"""
-    boxes = []
+    """n은 읽어야 하는 상자수
+각 상자는 액션 피규어의 높이를 담은 리스트"""
+    boxes = [ ]
     for i in range(n):
         box = input().split()
-        box = list(map(int, box[1:]))  # Convert all elements except the first to integers
+        box.pop(0)
+        for i in range(len(box)):
+            box[i] = int(box[i])
         boxes.append(box)
     return boxes
 
 def box_ok(box):
-    """box는 해당 상자 내부의 액션 피규어들의 높이를 담은 리스트이고,
-    True를 반환하면 상자 내 액션 피규어들의 높이의 순서가 작아지지 않는 것이고,
+    """ box는 해당 상자 내부의 액션 피규어들의 높이를 담은 리스트이고,
+    True를 반환하면 상자 내 액션 피규어들으 ㅣ높이의 순서가 작아지지 않는 것이고,
     False는 그렇지 않은 것이다"""
 
-    for i in range(len(box) - 1):
-        if box[i] > box[i + 1]:
-            return False
+    for i in range(len)box) -1):
+        if box[i] > box[i + i]:
+    return False
     return True
 
 def all_boxes_ok(boxes):
-    """boxes는 상자들의 리스트, 각 상자는 액션피규어 높이들의 리스트
+    """ boxes는 삼자들의 리스트, 각 상자는 액션피규어 높이들의 리스트
     상자내의 액션 피규어 높이가 점점작아지지 않으면 True를 반환, 그렇지 않으면 False"""
     for box in boxes:
         if not box_ok(box):
@@ -26,7 +29,7 @@ def all_boxes_ok(boxes):
     return True
 
 def boxes_endpoints(boxes):
-    """각각 두 개의 값이 있는 리스트들을 값으로 갖는 리스트를 반환. 두 개의 값은 상자의 가장 왼쪽과 가장 오른쪽 액션 피규어의 높이"""
+    """각각 두개의 값이 있는 리스트들을 값으로 갖는 리스트를 반화 두개의 값은 상자의 가장 왼쪽과 가장 오른쪽 액션 피규어의 높이"""
     endpoints = []
     for box in boxes:
         endpoints.append([box[0], box[-1]])
@@ -35,11 +38,12 @@ def boxes_endpoints(boxes):
 def all_endpoints_ok(endpoints):
     """endpoints는 피규어의 높이로 정렬되어야 함
      endpoints가 순서대로 정리가 가능한 상자면 True, 그렇지 않으면 False"""
-    maximum = endpoints[0][1]
-    for i in range(1, len(endpoints)):
-        if endpoints[i][0] < maximum:
-            return False
-        maximum = endpoints[i][1]
+
+maximum = endpoints[0][1]
+for i in range(1, len(endpoints)):
+    if endpoints[i][0] < maximum:
+        return False
+    maximum = endpoints[i][1]
     return True
 
 # 입력을 읽음
@@ -49,6 +53,7 @@ boxes = read_boxes(n)
 # 모든 상자가 정상인지 확인
 if not all_boxes_ok(boxes):
     print('NO')
+
 else:
     endpoints = boxes_endpoints(boxes)
     endpoints.sort()
@@ -57,3 +62,5 @@ else:
         print('YES')
     else:
         print('NO')
+
+
